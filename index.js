235b22b3c247ac48568b1d15e1cbdff73bdaf1db
Metaphore28 <raf.duhamel@gmail.com>
2022-08-17 14:30:25 -0400

    ajout de personne

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('Salut tout le monde!')
+
+const personne = require('./personne');
+// import personne from './personne';
+console.log(personne.nom);
+console.log("Voice l'age de Bob " + personne.age + " ans");
+
+
